cdb38eba0eca56a124aabb9790a3ec69e58ccc73
Janith S <161787933+Janith303@users.noreply.github.com>
2025-03-22 16:36:15 +0530

    front end main files

---
+++ frontend/src/Component/bottomnav/foter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../bottomnav/foter.css"
+
+function foter() {
+  return (
+    <foter className='custom-footer'>
+      <p>©2025 SMN Sync 360 G3.<br/>
+      All rights reserved.</p>
+    </foter>
+  )
+}
+
+export default foter
