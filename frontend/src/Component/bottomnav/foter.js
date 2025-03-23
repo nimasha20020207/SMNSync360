ac5768eb806b623e9ce779dc4fc2b3f99484c791
Janith S <161787933+Janith303@users.noreply.github.com>
2025-03-23 20:29:25 +0530

    final update in frontend
    Now this is ready to merge for the development branch
    add all the front-end and back end basic required file for

--- frontend/src/Component/bottomnav/foter.js
+++ frontend/src/Component/bottomnav/foter.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import "../bottomnav/foter.css"
+import React from "react";
+import "../bottomnav/foter.css";
 
 function foter() {
   return (
-    <div className='custom-footer'>
-      <p>©2025 SMN Sync 360 G3.<br/>
-      All rights reserved.</p>
+    <div className="custom-footer">
+      <p>
+        ©2025 SMN Sync 360 G3.
+        <br />
+        All rights reserved.
+      </p>
     </div>
-  )
+  );
 }
 
-export default foter
+export default foter;
