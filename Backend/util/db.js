b74b17d6ffe5ef96f7890d5a06c92b53ae35d7cd
nimasha20020207 <nimashakarunarathne61@gmail.com>
2025-05-04 19:36:24 +0200

    image names updated and db connection changed

--- Backend/util/db.js
+++ Backend/util/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
 //const db = mongoose.connect("mongodb://0.0.0.0:27017/CCMS");
-const db = mongoose.connect("mongodb+srv://janithsdissanayaka:ajtdQYIXjaiZbNli@cluster0.n2zxb.mongodb.net/");
+//const db = mongoose.connect("mongodb+srv://janithsdissanayaka:ajtdQYIXjaiZbNli@cluster0.n2zxb.mongodb.net/");
 
+
+const db=mongoose.connect("mongodb+srv://admin:XPXnccQFdUXAu3S@cluster0.e3evh.mongodb.net/");
 
 
 module.exports = db;
