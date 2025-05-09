c4e92b8ea510a9aa9c3e28b5e59d7dc4513bf88e
Janith S <161787933+Janith303@users.noreply.github.com>
2025-05-09 22:28:08 +0530

    add FO front end

--- frontend/src/Component/topNav/Header.js
+++ frontend/src/Component/topNav/Header.js
@@ -11,17 +11,17 @@ import { useNavigate } from "react-router-dom";
 function HeadNav() {
   const navigate = useNavigate();
   const [userId, setUserId] = useState("User");
-   const [userIds, setUserIds] = useState("userids");
+  const [userIds, setUserIds] = useState("userids");
   // const [userIdu, setUserIdu] = useState("Userid");
-  
-    useEffect(() => {
-      // Retrieve userId from localStorage
-      const storedUserId = localStorage.getItem("username");
-      const storeuserIds = localStorage.getItem("userids");
-      setUserId(storedUserId || "User"); // Fallback to "User" if not found
-      setUserIds(storeuserIds)
-    }, []);
-  
+
+  useEffect(() => {
+    // Retrieve userId from localStorage
+    const storedUserId = localStorage.getItem("username");
+    const storeuserIds = localStorage.getItem("userids");
+    setUserId(storedUserId || "User"); // Fallback to "User" if not found
+    setUserIds(storeuserIds);
+  }, []);
+
   const handleLogout = () => {
     localStorage.clear();
     sessionStorage.clear();
@@ -42,12 +42,24 @@ function HeadNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="he-navbar-nav">
-            <Nav.Link href="/pmhome" className="he-home">Home</Nav.Link>
-            <Nav.Link href="/ScheduleProjectDetails" className="he-home">project</Nav.Link>
-            <Nav.Link href="/AssignedTasks" className="he-home">task</Nav.Link>
-            <Nav.Link href="/Progress" className="he-users">Progress</Nav.Link>
-            <Nav.Link href="/Users" className="he-users">Requiment</Nav.Link>
-            <Nav.Link href="/Inventory" className="he-users">Inventory</Nav.Link>
+            <Nav.Link href="/pmhome" className="he-home">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/ScheduleProjectDetails" className="he-home">
+              project
+            </Nav.Link>
+            <Nav.Link href="/AssignedTasks" className="he-home">
+              task
+            </Nav.Link>
+            <Nav.Link href="/Progress" className="he-users">
+              Progress
+            </Nav.Link>
+            <Nav.Link href="/Users" className="he-users">
+              Requiment
+            </Nav.Link>
+            <Nav.Link href="/Inventory" className="he-users">
+              Inventory
+            </Nav.Link>
           </Nav>
 
           <Nav className="ms-auto">
@@ -59,7 +71,9 @@ function HeadNav() {
               }
               id="basic-nav-dropdown"
             >
-              <NavDropdown.Item as={Link} to={`/profile/${userIds}`}>My Account</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={`/profile/${userIds}`}>
+                My Account
+              </NavDropdown.Item>
               <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
