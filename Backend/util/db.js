58609517d6647e77b8328979d7f96d3b60d0ff3d
Janith S <161787933+Janith303@users.noreply.github.com>
2025-05-10 14:55:28 +0530

    correct db util

--- Backend/util/db.js
+++ Backend/util/db.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose")
 //const db = mongoose.connect("mongodb://0.0.0.0:27017/CCMS");
 const db = mongoose.connect("mongodb+srv://janithsdissanayaka:ajtdQYIXjaiZbNli@cluster0.n2zxb.mongodb.net/");
 
-
-//const db=mongoose.connect("mongodb+srv://admin:2jJAe9D2xykYSgQF@cluster0.e3evh.mongodb.net/");//nimasha-db
-
-
 module.exports = db;
 
 //ajtdQYIXjaiZbNli
